deca11167fb2e06a3c2f694bc46cd36aff74646a
joy <ciftekharulislam@gmail.com>
2021-08-25 00:42:29 +0600

    updated all component

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -7,10 +7,20 @@ import './NewExpense.css';
 
 // use rsc shortcut for arrow function
 
-const NewExpense = () => {
+const NewExpense = (props) => {
+
+    const saveExpenseDataHandler = (enteredExpenseData) => {
+        
+        const expenseData = {
+            ...enteredExpenseData,
+            id: Math.random().toString()
+        };
+        props.onAddExpense(expenseData);
+    };
+
     return (
         <div className="new-expense">
-            <ExpenseForm/>
+            <ExpenseForm onSaveExpenseData={saveExpenseDataHandler}/>
         </div>
     );
 };
